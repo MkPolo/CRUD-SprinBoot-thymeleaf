6c335bf441a39c02c8157408b8ff790c963d0e89
MkPolo <mk.polo1708@gmail.com>
2020-04-23 13:08:06 -0500

    Implementamos las clases -ciudad- -cliente- en el package Entity

---
+++ src/main/java/com/mkpolo/clienteapp/entity/Cliente.java
@@ -0,0 +1,94 @@
+package com.mkpolo.clienteapp.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clientes")
+public class Cliente implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	private String nombres;
+
+	private String apellidos;
+
+	private String telefono;
+
+	private String email;
+
+	@ManyToOne
+	@JoinColumn(name="cuidades_id")
+	private Ciudad ciudad;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombres=" + nombres + ", apellidos=" + apellidos + ", telefono=" + telefono
+				+ ", email=" + email + ", ciudad=" + ciudad + "]";
+	}
+	
+	
+	
+	}
