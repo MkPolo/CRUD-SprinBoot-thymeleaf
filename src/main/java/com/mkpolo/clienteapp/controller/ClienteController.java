6ea5092444630e9cea3891aa18550276b9e2d068
MkPolo <mk.polo1708@gmail.com>
2020-04-24 21:57:33 -0500

    Validación de errores en Edit y Delete

--- src/main/java/com/mkpolo/clienteapp/controller/ClienteController.java
+++ src/main/java/com/mkpolo/clienteapp/controller/ClienteController.java
@@ -25,70 +25,93 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
-	
+
 	@Autowired
 	private ICiudadService ciudadService;
-	
+
 	@GetMapping("/")
 	public String listarClientes(Model model) {
 		List<Cliente> listadoClientes = clienteService.listartodos();
-		
-		model.addAttribute("titulo","Lista de Clientes");
+
+		model.addAttribute("titulo", "Lista de Clientes");
 		model.addAttribute("clientes", listadoClientes);
-		
+
 		return "/views/clientes/listar";
 	}
-	
+
 	@GetMapping("/create")
 	public String crear(Model model) {
-		
+
 		Cliente cliente = new Cliente();
 		List<Ciudad> listCiudades = ciudadService.listarCiudad();
-		
-		model.addAttribute("titulo","Formulario: Nuevo Cliente");
-		model.addAttribute("cliente",cliente);
-		model.addAttribute("ciudades",listCiudades);
-		
+
+		model.addAttribute("titulo", "Formulario: Nuevo Cliente");
+		model.addAttribute("cliente", cliente);
+		model.addAttribute("ciudades", listCiudades);
+
 		return "/views/clientes/frmCrear";
 	}
-	
+
 	@PostMapping("/save")
 	public String guardar(@Valid @ModelAttribute Cliente cliente, BindingResult result, Model model) {
-		
+
 		List<Ciudad> listCiudades = ciudadService.listarCiudad();
-		
+
 		if (result.hasErrors()) {
-			
-			model.addAttribute("titulo","Formulario: Nuevo Cliente");
-			model.addAttribute("cliente",cliente);
-			model.addAttribute("ciudades",listCiudades);
-			
+
+			model.addAttribute("titulo", "Formulario: Nuevo Cliente");
+			model.addAttribute("cliente", cliente);
+			model.addAttribute("ciudades", listCiudades);
+
 			return "/views/clientes/frmCrear";
 		}
-		
+
 		clienteService.guardar(cliente);
 		return "redirect:/views/clientes/";
 	}
-	
+
 	@GetMapping("/edit/{id}")
 	public String editar(@PathVariable("id") Long idCliente, Model model) {
-		
-		Cliente cliente = clienteService.buscarPorId(idCliente);
+
+		Cliente cliente = null;
+
+		if (idCliente > 0) {
+			cliente = clienteService.buscarPorId(idCliente);
+
+			if (cliente == null) {
+				return "redirect:/views/clientes/";
+			}
+		} else {
+			return "redirect:/views/clientes/";
+		}
+
 		List<Ciudad> listCiudades = ciudadService.listarCiudad();
-		
-		model.addAttribute("titulo","Formulario: Editar Cliente");
-		model.addAttribute("cliente",cliente);
-		model.addAttribute("ciudades",listCiudades);
-		
+
+		model.addAttribute("titulo", "Formulario: Editar Cliente");
+		model.addAttribute("cliente", cliente);
+		model.addAttribute("ciudades", listCiudades);
+
 		return "/views/clientes/frmCrear";
 	}
-	
+
 	@GetMapping("/delete/{id}")
 	public String eliminar(@PathVariable("id") Long idCliente) {
-		
+
+		Cliente cliente = null;
+
+		if (idCliente > 0) {
+			cliente = clienteService.buscarPorId(idCliente);
+
+			if (cliente == null) {
+				return "redirect:/views/clientes/";
+			}
+		} else {
+			return "redirect:/views/clientes/";
+		}
+
 		clienteService.eliminar(idCliente);
-		
+
 		return "redirect:/views/clientes/";
 	}
-	
+
 }
